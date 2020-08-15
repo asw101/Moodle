32d27577743c6091ad17c52b5292f14bb6373fb7
Aaron Wislang <aaron.wislang@microsoft.com>
2020-08-15 02:00:24 +0000

    add cloud-init_*

---
+++ scripts/cloud-init_controller.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+git clone -b asw101-dev https://github.com/asw101/Moodle
+cd /home/azureadmin/Moodle/scripts/
+apt-get update && apt-get install -y moreutils
+
+# install_moodle
+bash install_moodle.sh /home/azureadmin/env.json 2>&1 | ts '%Y%m%d-%H:%M:%S' | tee install_moodle.log
+
